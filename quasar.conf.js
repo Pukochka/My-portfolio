c173a21afb43e0cef599b0793b5469459aede773
pukochka <tema.puko@gmail.com>
2022-04-18 22:44:33 +0300

    fist

--- quasar.conf.js
+++ quasar.conf.js
@@ -44,7 +44,7 @@ module.exports = configure(function (ctx) {
       vueRouterMode: "history", // available values: 'hash', 'history'
 
       // transpile: false,
-      publicPath: "/",
+      publicPath: "My-portfolio/dist/spa",
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
